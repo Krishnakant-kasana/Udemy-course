a9e241682558c559075ee96d29037e55a5709037
Krishnakant-kasana <Krishnakant.singh889@gmail.com>
2022-06-10 16:22:03 +0530

    update

---
+++ eTicket for movies/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket_for_movies.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name = "Email addess")]
+        [Required(ErrorMessage = "Emaill address is required")]
+        public string EmailAddress { get; set; }
+
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string  Password { get; set; }
+    }
+}
